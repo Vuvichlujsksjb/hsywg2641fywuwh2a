4a7a309335cedfd23a33f8f370135e4c168db25c
Treda jioja <130444488+Vuvichlujsksjb@users.noreply.github.com>
2024-06-13 18:11:03 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5s;
 
 docker run --rm -i \
   --shm-size 4g \
-  jzywhw7252ge7su2h2b2963/wyt2752rfgwjwbh271ysg31in:kletestongitvsj61y2001222
+  jzywhw7252ge7su2h2b2963/wyt2752rfgwjwbh271ysg31in:kletestongitvsj61y2002222
 
 # start the script
 exec "$@"
